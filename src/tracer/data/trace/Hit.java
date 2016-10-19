a23df64732fb5f2b82102e845795eb69ca9025fe
Pedro Henrique <pedro00dk@live.com>
2016-10-18 21:47:31 -0300

    Create the Ray and Hit classes

---
+++ src/tracer/data/trace/Hit.java
@@ -0,0 +1,55 @@
+package tracer.data.trace;
+
+import tracer.data.Vector3;
+import tracer.data.material.Color;
+
+/**
+ * Represents a hit information about a ray casting in a model. Instances of this class should only be created when a
+ * hit happens. The objects of this class has little life time (as the Ray class),  so the access to the fields are
+ * public, be careful with null fields and improper modifications.
+ *
+ * @author Pedro Henrique
+ * @see Ray
+ */
+public class Hit {
+
+    /**
+     * The ray used in the intersection calculation information.
+     */
+    public Ray ray;
+
+    /**
+     * The ray direction scalar, used to calculate the hitPoint.
+     */
+    public float rayDirectionScalar;
+
+    /**
+     * The point where the ray touches the model.
+     */
+    public Vector3 hitPoint;
+
+    /**
+     * The model normal in the hit point (should be normalized).
+     */
+    public Vector3 hitNormal;
+
+    /**
+     * The calculated color in the hit point.
+     */
+    public Color hitColor;
+
+    /**
+     * @param ray                the ray used.
+     * @param rayDirectionScalar the ray direction scalar to generate the hitPoint.
+     * @param hitPoint           the model hit point.
+     * @param hitNormal          the normal in the hit point (should be normalized).
+     * @param hitColor           the calculated color in the hit point.
+     */
+    public Hit(Ray ray, float rayDirectionScalar, Vector3 hitPoint, Vector3 hitNormal, Color hitColor) {
+        this.ray = ray;
+        this.rayDirectionScalar = rayDirectionScalar;
+        this.hitPoint = hitPoint;
+        this.hitNormal = hitNormal;
+        this.hitColor = hitColor;
+    }
+}
---
+++ src/tracer/data/trace/Ray.java
@@ -0,0 +1,35 @@
+package tracer.data.trace;
+
+import tracer.data.Vector3;
+
+/**
+ * Represents a ray, with an origin and direction vectors, this vectors are used in hit checking. Due the low lifetime
+ * of the objects of this class, the access can be done directly in the instance fields.
+ *
+ * @author Pedro Henrique
+ */
+public class Ray {
+
+    /**
+     * The origin of this ray.
+     */
+    public Vector3 origin;
+
+    /**
+     * The direction of this ray, is normalized in the ray initialization.
+     */
+    public Vector3 direction;
+
+    /**
+     * Creates a ray with the received origin and direction vectors, the vectors are not copied, the vector instances
+     * should be controlled by other application levels, it can provide possible performance improvements. Ensure the
+     * direction has the magnitude greater than 0.
+     *
+     * @param origin    the origin of this ray
+     * @param direction the direction of this ray, the direction magnitude should be greater than 0
+     */
+    public Ray(Vector3 origin, Vector3 direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+}
